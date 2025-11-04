a94c3323c2287af7956e4a8a601bc7d77baadcb8
Franco <fjimenez@mobydigital.com>
2025-11-04 11:26:45 -0300

    refactor(auth): convert Config class to record [AMA-174]

--- src/main/java/com/microservicios/api_gateway/config/CustomAuthGatewayFilterFactory.java
+++ src/main/java/com/microservicios/api_gateway/config/CustomAuthGatewayFilterFactory.java
@@ -123,15 +123,6 @@ public class CustomAuthGatewayFilterFactory extends AbstractGatewayFilterFactory
         return ErrorResponseBuilder.buildErrorResponse(exchange.getResponse(), status, message);
     }
 
-    public static class Config {
-        private List<String> excludePaths;
-
-        public List<String> getExcludePaths() {
-            return excludePaths;
-        }
-
-        public void setExcludePaths(List<String> excludePaths) {
-            this.excludePaths = excludePaths;
-        }
+    public record Config(List<String> excludePaths) {
     }
 }
